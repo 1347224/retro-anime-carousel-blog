f7d3cef3f565a766ffe4c86e0e4f8f67f0b2b703
1347224 <marioalfredo.garciabedoya3@gmail.com>
2025-05-22 05:21:57 -0500

    Update TopList.tsx

--- src/pages/TopList.tsx
+++ src/pages/TopList.tsx
@@ -282,7 +282,7 @@ const TopList = () => {
               >
                 <CarouselContent className="-ml-4">
                   {topOpenings.map((opening) => (
-                    <CarouselItem key={opening.rank} className="pl-2 md:basis-1/4">
+                    <CarouselItem key={opening.rank} className="pl-4 md:basis-1/3">
                       <TopOpeningCard
                         rank={opening.rank}
                         title={opening.title}
